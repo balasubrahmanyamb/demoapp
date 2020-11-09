3b9004524decb58a3db2cc1c7bc6da97450aa103
balasubrahmanyam.b <balasubrahmanyam.b@valtech.com>
2020-11-09 13:24:45 +0530

    Add project files.

---
+++ demoapp/demoapp.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class demoapp
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
